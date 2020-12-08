3318ec2af981465c308858ee0d7a61b7f055ad32
uribon1985 <s07250503@gmail.com>
2020-12-08 15:02:04 +0900

    商品出品機能の実装４

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,15 +1,15 @@
 FactoryBot.define do
   factory :item do
     name                    {'ddd'}
-    category_id             {'3'}
-    product_condition_id    {'2'}
-    shipping_charges_id     {'2'}
-    shipping_area_id        {'11'}
-    estimated_shipping_id   {'4'}
+    category_id             {3}
+    product_condition_id    {2}
+    shipping_charges_id     {2}
+    shipping_area_id        {11}
+    estimated_shipping_id   {4}
     product_description     {'ddddjjjj'}
-    price                   {'500'}
+    price                   {500}
     association :user
-    
+
     after(:build) do |item|
       item.image.attach(io: File.open('public/images/flag.png'), filename: 'flag.png')
     end
