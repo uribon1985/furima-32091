7a8bf6423905e1ad212968abd308be1466296d75
uribon1985 <s07250503@gmail.com>
2020-12-15 20:02:24 +0900

    商品詳細表示機能の実装3

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -22,11 +22,11 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
-  def destroy
-    item = Item.find(params[:id])
-    item.destroy
-    redirect_to root_path
-  end
+  #def destroy
+    #item = Item.find(params[:id])
+    #item.destroy
+    #redirect_to root_path
+  #end
 
   private
   def item_params
