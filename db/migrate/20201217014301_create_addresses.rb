d2f02025a8aa38cc548e697bed894e02856b10a2
uribon1985 <s07250503@gmail.com>
2020-12-20 16:39:51 +0900

    マイグレーションファイルの変更

--- db/migrate/20201217014301_create_addresses.rb
+++ db/migrate/20201217014301_create_addresses.rb
@@ -5,7 +5,7 @@ class CreateAddresses < ActiveRecord::Migration[6.0]
       t.integer    :shipping_area_id,  null:false
       t.string     :municipality,      null:false
       t.string     :address ,          null:false
-      t.string     :building_name,     
+      t.string     :building_name     
       t.string     :phone_number,      null:false
       t.references :order ,            null:false, foreign_key: true
       t.timestamps
